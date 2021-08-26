e645011a8dfc5c5b5121daf8617d9c9e4af5cec7
caiohscruz <caiohscruz@hotmail.com>
2021-08-26 14:46:19 -0300

    Inclui comportamento quando x^2 estiver envolvido

--- public/scrypts/numbers.js
+++ public/scrypts/numbers.js
@@ -1,20 +1,38 @@
 // tratamento dos botoes de numerais
+
+// identificando todos os botões de números, para os quais atribui a
+// classe "num"
 const numButtons = document.querySelectorAll(".num") 
 
+// adicionando um evento de clique a esses botões
 numButtons.forEach(button => {
-    // adicionar escuta 
     button.addEventListener("click", function() {numClick(button.getAttribute("id"))})
 })
+
+// função que insere os números no visor
 function numClick(id){
-    if (document.getElementById("visor2").value=="0"||document.getElementById("control").value=="true"){
-        document.getElementById("visor2").value=id
-        document.getElementById("control").value="false"
+    const visorInferior = document.getElementById("visor2")
+    const visorInferiorIsResult = document.getElementById("visor2IsResult")
+    const visorSuperior = document.getElementById("visor1")
+    
+    // se o botão "x²" foi clicado antes, mas o botão "=" não foi clicado na sequência,
+    // ao clicar em qualquer número, sqrAux deve ser resetado e o visorSuperior de ser
+    // alterado de forma a tirar o "sqr(x)"
+    const auxSqr = document.getElementById("auxSqr")
+
+    if (auxSqr.value!=""){
+        visorSuperior.value = visorSuperior.value.replace(auxSqr.value,"")
+        auxSqr.value=""
+    }    
+    
+    // caso o "visor2" contenha apenas o valor 0 ou o resultado
+    // de uma operação anterior, ao clicar em algum número, o visor2
+    // precisa ser atualizado para o valor do botão clicado, senão
+    // o valor do visor deve ser concatenado com o valor do botão
+    if (visorInferior.value=="0"||visorInferiorIsResult.value=="true"){
+        visorInferior.value=id
+        visorInferiorIsResult.value="false"
     } else {
-        document.getElementById("visor2").value+=id
+        visorInferior.value+=id
     }
 }
-/*
-    Percebi que numa calculadora, ao realizar diversas operações seguidas, o resultado
-    vai para o "visor" de baixo, mas, ao clicar em algum numero em seguida, o visor de 
-    baixo passa a ter aquele valor. O "control" aqui serve para replicar esse comportamento
-*/
