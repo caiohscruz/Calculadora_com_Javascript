fd1141b84e6d69da7cef218ce4a79a6d4478fbbf
caiohscruz <caiohscruz@hotmail.com>
2021-08-26 22:04:16 -0300

    Add tratamento para raiz

---
+++ public/scrypts/root.js
@@ -0,0 +1,21 @@
+// tratamento do botao de raiz
+const rootButton = document.getElementById("root") 
+
+rootButton.addEventListener("click", function() {rootClick()})
+
+function rootClick(){
+    const visorInferior = document.getElementById("visor2")
+    const visorSuperior = document.getElementById("visor1")
+    const visorInferiorIsResult = document.getElementById("visor2IsResult")
+    
+    if (visorInferiorIsResult.value=="true"){
+        visorSuperior.value+="√("+visorSuperior.value+")"
+        visorInferior.value = Math.sqrt(eval(visorInferior.value))
+    }
+    if (visorInferiorIsResult.value=="false"){
+        visorSuperior.value+="√"+visorInferior.value
+        visorInferior.value = Math.sqrt(eval(visorInferior.value))
+        visorInferiorIsResult.value="true"
+    }
+    
+}
