4b6a59165ec6b291061535b453cc8369e17bccd9
caiohscruz <caiohscruz@hotmail.com>
2021-08-29 14:36:42 -0300

    feat(dot): Add dot button functionality

--- public/scrypts/numbers.js
+++ public/scrypts/numbers.js
@@ -78,7 +78,7 @@ function numClick(id){
             visorInferior.value="0"
             auxCalc.value=visorInferior.value
         }else{
-            visorSuperior.value=visorSuperior.value.replace(auxBuilderStr.value,"")
+            visorSuperior.value=visorSuperior.value.slice(0, - auxBuilderStr.value.length)
             if(visorSuperior.value.charAt(visorSuperior.value.length-1)=="+"){
                 auxCalc.value=eval(auxCalc.value+"-"+visorInferior.value)
             }else if(visorSuperior.value.charAt(visorSuperior.value.length-1)=="-"){
