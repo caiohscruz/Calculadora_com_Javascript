61b11374ae582e59c2f5053668b4b49142e90e51
caiohscruz <caiohscruz@hotmail.com>
2021-08-29 13:25:33 -0300

    feat(root): Add root functionality

--- public/scrypts/root.js
+++ public/scrypts/root.js
@@ -14,21 +14,21 @@ function rootClick(){
     const isResult = document.getElementById("isResult")
     /* "auxCalc" será utilizado para auxiliar nas operações */
     const auxCalc = document.getElementById("auxCalc")
-    /* "auxBuilderStr" será utilizado para construir a string "sqr(x)" */
+    /* "auxBuilderStr" será utilizado para construir a string "√(x)" */
     const auxBuilderStr = document.getElementById("auxBuilderStr")   
     /* "operacoes" guarda os simbolos dos operadores básicos. Como farei o uso da funçao "eval"
     terei eventualmente que lidar com os simbolos de multiplicação e divisão com "replace" */
     const operacoes = ["+","-","x","÷"]
     
-    /* Se auxBuilderStr está vazio, é porque a última operação não foi tratar do x², então
-    auxBuilderStr tem que ser ser setado com "sqr(x)" onde x é o valor do visorInferior, e
+    /* Se auxBuilderStr está vazio, é porque a última operação não foi tratar do x² ou raiz ou +/-,
+    então auxBuilderStr tem que ser ser setado com "√(x)" onde x é o valor do visorInferior, e
     essa informação deve ser setada no visorSuperior */
     
     /* Se o visorSuperior estiver vazio, é porque não há operações anteriores a considerar.
     "auxCalc" guardará o valor de x² para operações futuras */
     if((visorSuperior.value=="")&&(auxBuilderStr.value=="")){
-        auxBuilderStr.value="sqr("+visorInferior.value+")"
-        visorInferior.value=eval(visorInferior.value+"*"+visorInferior.value)
+        auxBuilderStr.value="√("+visorInferior.value+")"
+        visorInferior.value=Math.sqrt(eval(visorInferior.value))
         visorSuperior.value=auxBuilderStr.value
         auxCalc.value=visorInferior.value
     }else{
@@ -36,21 +36,21 @@ function rootClick(){
         matemática, houve operações antes, então não podemos desconsiderar o auxCalc, que nos ajuda
         ir realizando os cálculo */
         if(operacoes.includes(visorSuperior.value.charAt(visorSuperior.value.length-1))){
-            auxBuilderStr.value="sqr("+visorInferior.value+")"
-            visorInferior.value=eval(visorInferior.value+"*"+visorInferior.value)
+            auxBuilderStr.value="√("+visorInferior.value+")"
+            visorInferior.value=Math.sqrt(eval(visorInferior.value))
             auxCalc.value=eval(auxCalc.value+visorSuperior.value.charAt(visorSuperior.value.length-1).replace("x","*").replace("÷","/")+visorInferior.value)
             visorSuperior.value+=auxBuilderStr.value
         }else{
             /* Se auxBuilderStr não está vazio, é porque a última operação foi tratar do x², então
-            auxBuilderStr tem que ser ser setado com "sqr(auxBuilderStr)", e essa informação deve ser setada
+            auxBuilderStr tem que ser ser setado com "√(auxBuilderStr)", e essa informação deve ser setada
             no visorSuperior */
             if (auxBuilderStr.value!=""){
                 /* Se retirar o auxBuilderStr do visorSuperior fez ele ficar vazio, então não temos que nos
                 preocupar com calculos anteriores */
                 visorSuperior.value=visorSuperior.value.replace(auxBuilderStr.value,"")
-                auxBuilderStr.value="sqr("+auxBuilderStr.value+")"
+                auxBuilderStr.value="√("+auxBuilderStr.value+")"
                 if(visorSuperior.value==""){
-                    auxCalc.value=eval(auxCalc.value+"*"+auxCalc.value)
+                    auxCalc.value=Math.sqrt(eval(visorInferior.value))
                     visorSuperior.value=auxBuilderStr.value
                     visorInferior.value=auxCalc.value
                 }else{
@@ -65,7 +65,7 @@ function rootClick(){
                     }else{
                         auxCalc.value=eval(auxCalc.value+"*"+visorInferior.value)
                     }
-                    visorInferior.value=eval(visorInferior.value+"*"+visorInferior.value)
+                    visorInferior.value=Math.sqrt(eval(visorInferior.value))
                     auxCalc.value=eval(auxCalc.value+visorSuperior.value.charAt(visorSuperior.value.length-1).replace("x","*").replace("÷","/")+visorInferior.value)
                     visorSuperior.value+=auxBuilderStr.value
                 }
