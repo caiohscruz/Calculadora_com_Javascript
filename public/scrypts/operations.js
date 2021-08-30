633d5f393311a9469526e78026eb3d7547a33f26
caiohscruz <caiohscruz@hotmail.com>
2021-08-30 11:23:57 -0300

    Atualiza comentários do script

--- public/scrypts/operations.js
+++ public/scrypts/operations.js
@@ -1,22 +1,8 @@
-/* Aqui serão tratados os botões das operações básicas (+,-.*,/) */ 
+/* Aqui serão tratados os botões das operações básicas (+,-,x,÷) */ 
 
-/* O trecho abaixo é responsável por identificar todos os botões dessas operações,
-para os quais atribui a classe "simple_operation", e adicionar a eles uma escula 
-para o evento de clique que chamará a função correspondente */ 
+/* O trecho abaixo é responsável por identificar todos os botões dessas operações
+e adicionar a eles uma escuta para o evento de clique que chamará a função correspondente */ 
 
-/*
-const sOperButtons = document.querySelectorAll(".simple_operation") 
-sOperButtons.forEach(button => {
-    button.addEventListener("click", function() {sOperClick(button.getAttribute("id"))})
-})
-*/
-
-/* Numa discussão no linkedin, comentaram que usar um forEach para isso não seria 
-interessante, seria um incremento de complexidade sem benefícios, que seria melhor, 
-por uma questão de desempenho, acessar elemento por elemento diretamente, em vez de 
-por meio de um laço de repetição. Por isso o código acima foi comentado, e adiciono
-o que segue. Resolvi acatar a sugestão deles por não ter ainda muita experiência,
-espero ter propriedade para decidir essas coisa em não muito tempo */
 const button_plus = document.getElementById("plus")
 const button_minus = document.getElementById("minus")
 const button_times = document.getElementById("times")
@@ -28,9 +14,7 @@ button_times.addEventListener("click", function() {sOperClick("times")})
 button_div.addEventListener("click", function() {sOperClick("div")})
 
 function sOperClick(oper){
-    /* A constante abaixo se refere a um input que não ficará visível ao usuário
-    que servirá para armazenar uma referência de qual foi o último botão de 
-    operação clicado */
+    
     const operador = document.getElementById("oper")
     /* A função de clique passa o "id" do botão clicado como parâmetro. Essa
     informação vai ser tratada pelo trecho abaixo, que irá atribuir a "operador"
@@ -54,21 +38,12 @@ function sOperClick(oper){
         }
     }
     
-    /* visorSuperior e visorInferior correpondem aos campos do visor da calculadora */
     const visorSuperior = document.getElementById("visor1")
     const visorInferior = document.getElementById("visor2")
-    /* isResult sinaliza se o conteúdo do visorInferior é ou não resultado de
-    alguma operação anterior */
     const isResult = document.getElementById("isResult")
-    /* "auxCalc" será utilizado para auxiliar nas operações */
     const auxCalc = document.getElementById("auxCalc")
-    /* "auxBuilderStr" guarda uma string do tipo "sqr(x)" e aqui vai indicar que anteriormente
-    houve o tratamento do botão x² */
     const auxBuilderStr = document.getElementById("auxBuilderStr")
-    /* div0 indica se houve uma divisão por zero anteriormente  */
     const div0 = document.getElementById("div0")
-    /* "operacoes" guarda os simbolos dos operadores básicos. Como farei o uso da funçao "eval"
-    terei eventualmente que lidar com os simbolos de multiplicação e divisão com "replace" */
     const operacoes = ["+","-","x","÷"]
     
     
@@ -99,7 +74,7 @@ function sOperClick(oper){
                 visorSuperior.value=visorSuperior.value.replace("x","*")
                 auxCalc.value=eval(visorSuperior.value)
                 /* Setando os visores */
-                if((auxCalc.value!="Infinity")&&(auxCalc.value!="NaN")){
+                if((auxCalc.value!="Infinity")&&(auxCalc.value!="-Infinity")&&(auxCalc.value!="NaN")){
                     visorSuperior.value=auxCalc.value+operador.value
                     visorInferior.value=auxCalc.value
                 }else{
@@ -108,7 +83,7 @@ function sOperClick(oper){
                     visorSuperior.value=visorSuperior.value+operador.value
                     div0.value="true"
                     document.getElementById("disableButton").click()
-                    if (auxCalc.value=="Infinity") {
+                    if ((auxCalc.value=="Infinity")||(auxCalc.value=="-Infinity")) {
                         visorInferior.value="Não é possível dividir por zero"
                         visorInferior.classList.add("smallertext")
                     } else if (auxCalc.value=="NaN"){
@@ -122,7 +97,7 @@ function sOperClick(oper){
             /* Se o visorSuperio não está vazio e não termina um símbolo de operação matemática,
             devemos estar com o quadrado ou raiz de um número no visor. Devemos então concatenar
             a informação do visor com o simbolo do operador e setar o valor da auxCalc */
-            if((visorSuperior.value.replace(auxBuilderStr.value,"")=="")||(visorSuperior.value.replace(auxBuilderStr.value,"")=="-")){
+            if(visorSuperior.value.replace(auxBuilderStr.value,"")==""){
                 visorSuperior.value+=operador.value
                 auxCalc.value=visorInferior.value
             }else{
