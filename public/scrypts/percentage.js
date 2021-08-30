9f5a5f8fad11d1ebac0f0250dd4172f3025ff076
caiohscruz <caiohscruz@hotmail.com>
2021-08-30 11:39:08 -0300

    Adiciona comentários ao script e altera concatenacao para boas praticas

--- public/scrypts/percentage.js
+++ public/scrypts/percentage.js
@@ -1,3 +1,6 @@
+/* Tratamento do botão % */
+
+/* Mapeando o botão e lhe atribuindo evento de clique */
 const perceButton = document.getElementById("%") 
 perceButton.addEventListener("click", function() {perceClick()})
 
@@ -9,6 +12,15 @@ function perceClick(){
     const auxCalc = document.getElementById("auxCalc")
     const plusOrMinus = ["+","-"]
     const timesOrDiv = ["x","÷"]    
+
+    /* O botão % exibe 3 comportamentos:
+    1- Caso só esteja setado um elemento para a operação, zera os visores;
+    2- Caso o visor superior já apresente uma expressão ou parte dela, com operador
+    de soma ou subtração, o segunto termo será calculado como sendo x% do primeiro, onde
+    x é o valor no visorInferior;
+    3- Caso o visor superior já apresente uma expressão ou parte dela, com operador
+    de multiplicação ou divisão, o segunto termo será calculado como sendo x/100, onde
+    x é o valor no visorInferior*/
     
     if(visorSuperior.value==""){
         visorSuperior.value="0"
@@ -20,31 +32,31 @@ function perceClick(){
         auxCalc.value="0"
         auxBuilderStr.value=""
     }else if(plusOrMinus.includes(visorSuperior.value.charAt(visorSuperior.value.length-1))){
-        visorInferior.value=eval(auxCalc.value+"*0.01*"+visorInferior.value)
-        auxCalc.value=eval(auxCalc.value+visorSuperior.value.charAt(visorSuperior.value.length-1)+"("+visorInferior.value+")")    
+        visorInferior.value=eval(`${auxCalc.value}*0.01*(${visorInferior.value})`)
+        auxCalc.value=eval(`${auxCalc.value}${visorSuperior.value.charAt(visorSuperior.value.length-1)}(${visorInferior.value})`)    
         visorSuperior.value+=visorInferior.value    
     }else if(timesOrDiv.includes(visorSuperior.value.charAt(visorSuperior.value.length-1))){
-        visorInferior.value=eval("0.01*"+visorInferior.value)
+        visorInferior.value=eval(`0.01*${visorInferior.value}`)
         auxCalc.value=eval(auxCalc.value+visorSuperior.value.charAt(visorSuperior.value.length-1).replace("÷","/").replace("x","*")+visorInferior.value)    
         visorSuperior.value+=visorInferior.value  
     }else if(auxBuilderStr.value!=""){
         visorSuperior.value=visorSuperior.value.slice(0, - auxBuilderStr.value.length)
         auxBuilderStr.value=""
         if(visorSuperior.value.charAt(visorSuperior.value.length-1)=="+"){
-            auxCalc.value=eval(auxCalc.value+"-"+visorInferior.value)
+            auxCalc.value=eval(`${auxCalc.value}-(${visorInferior.value})`)
         }else if(visorSuperior.value.charAt(visorSuperior.value.length-1)=="-"){
-            auxCalc.value=eval(auxCalc.value+"+"+visorInferior.value)
+            auxCalc.value=eval(`${auxCalc.value}+${visorInferior.value}`)
         }else if(visorSuperior.value.charAt(visorSuperior.value.length-1)=="x"){
-            auxCalc.value=eval(auxCalc.value+"/"+visorInferior.value)
+            auxCalc.value=eval(`${auxCalc.value}/${visorInferior.value}`)
         }else{
-            auxCalc.value=eval(auxCalc.value+"*"+visorInferior.value)
+            auxCalc.value=eval(`${auxCalc.value}*${visorInferior.value}`)
         }
         if(plusOrMinus.includes(visorSuperior.value.charAt(visorSuperior.value.length-1))){
-            visorInferior.value=eval(auxCalc.value+"*0.01*"+visorInferior.value)
-            auxCalc.value=eval(auxCalc.value+visorSuperior.value.charAt(visorSuperior.value.length-1)+"("+visorInferior.value+")")    
+            visorInferior.value=eval(`${auxCalc.value}*0.01*(${visorInferior.value})`)
+            auxCalc.value=eval(`${auxCalc.value}${visorSuperior.value.charAt(visorSuperior.value.length-1)}(${visorInferior.value})`)    
             visorSuperior.value+=visorInferior.value    
         }else if(timesOrDiv.includes(visorSuperior.value.charAt(visorSuperior.value.length-1))){
-            visorInferior.value=eval("0.01*"+visorInferior.value)
+            visorInferior.value=eval(`0.01*${visorInferior.value}`)
             auxCalc.value=eval(auxCalc.value+visorSuperior.value.charAt(visorSuperior.value.length-1).replace("÷","/").replace("x","*")+visorInferior.value)    
             visorSuperior.value+=visorInferior.value  
         }
