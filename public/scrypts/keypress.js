61d9a808dcb4dedf7af813d0f951fcdfc1164205
caiohscruz <caiohscruz@hotmail.com>
2021-08-30 11:06:17 -0300

    Adiciona comentários ao script

--- public/scrypts/keypress.js
+++ public/scrypts/keypress.js
@@ -1,3 +1,11 @@
+/* Este não é tão funcional, mas aqui trato do reconhecimento do evento de
+pressionar teclas para desencadear o clique dos botões correspondentes.
+Botões que possuem efeitos específicos no navegador não podem ser mapeados
+desta forma, por isso este scrypt acaba só lidando com os numerais, enter, 
+ponto, vírgula e operações matemáticas básicas */
+
+/* Adiciona ao documento escuta para o pressionar das teclas e atribui a esse evento
+um comportamento */
 document.addEventListener ('keypress', (event) => {
     const keyName = event.key;
     const refId = ["0","1","2","3","4","5","6","7","8","9","%"]
