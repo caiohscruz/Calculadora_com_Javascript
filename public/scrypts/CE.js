3b27665d6f6fb39463346baac197f0129d12e6af
caiohscruz <caiohscruz@hotmail.com>
2021-08-30 10:27:38 -0300

    Adiciona comentários ao script

--- public/scrypts/CE.js
+++ public/scrypts/CE.js
@@ -1,8 +1,10 @@
 /* Tratamento do botao CE */ 
-const CEButton = document.getElementById("CE") 
 
+/* Mapeando o botão e lhe atribuindo evento de clique */
+const CEButton = document.getElementById("CE") 
 CEButton.addEventListener("click", function() {CEClick()})
 
+/* O comportamento usual do CE é limpar o visorInferior */
 function CEClick(){
     
     const visorSuperior = document.getElementById("visor1")
@@ -13,31 +15,33 @@ function CEClick(){
     const auxCalc = document.getElementById("auxCalc")
     const operacoes = ["+","-","x","÷"]
     
-        if((visorSuperior.value.charAt(visorSuperior.value.length-1)=="=")||(div0.value=="true")){
-            visorSuperior.value=""
-            
-            /* Caso a informação na parte superior do visor termine com "=", o botão C 
-            se comporta como o CE, resetando a calculadora */
-            document.getElementById('C').click()
-            
-        }else if(auxBuilderStr!=""){
-            visorSuperior.value=visorSuperior.value.replace(auxBuilderStr.value, "")
-            
-            if(operacoes.includes(visorSuperior.value.charAt(visorSuperior.value.length-1))){
-                if(visorSuperior.value.charAt(visorSuperior.value.length-1)=="+"){
-                    auxCalc.value=eval(auxCalc.value+"-"+visorInferior.value)
-                }else if(visorSuperior.value.charAt(visorSuperior.value.length-1)=="-"){
-                    auxCalc.value=eval(auxCalc.value+"+"+visorInferior.value)
-                }else if(visorSuperior.value.charAt(visorSuperior.value.length-1)=="x"){
-                    auxCalc.value=eval(auxCalc.value+"/"+visorInferior.value)
-                }else{
-                    auxCalc.value=eval(auxCalc.value+"*"+visorInferior.value)
-                }   
-                auxBuilderStr.value=""         
-            }
-        }    
-        /* O comportamento usual do CE é limpar a parte inferior do visor */
-        visorInferior.value = "0"
-        isResult.value="false"
+    /* Mas, caso a informação na parte superior do visor termine com "=", ou 
+    tenha havido divisão por zero ou raiz quadrada de negativo antes, o botão CE 
+    deve resetar a calculadora */
+    if((visorSuperior.value.charAt(visorSuperior.value.length-1)=="=")||(div0.value=="true")){
+        visorSuperior.value=""
+        
+        document.getElementById('C').click()
+        
+    }else if(auxBuilderStr!=""){
+        /* Caso "auxBuilderStr" não esteja vazio, devemos ainda recalcular o "auxCalc" */
+
+        visorSuperior.value=visorSuperior.value.replace(auxBuilderStr.value, "")
+        
+        if(operacoes.includes(visorSuperior.value.charAt(visorSuperior.value.length-1))){
+            if(visorSuperior.value.charAt(visorSuperior.value.length-1)=="+"){
+                auxCalc.value=eval(auxCalc.value+"-"+visorInferior.value)
+            }else if(visorSuperior.value.charAt(visorSuperior.value.length-1)=="-"){
+                auxCalc.value=eval(auxCalc.value+"+"+visorInferior.value)
+            }else if(visorSuperior.value.charAt(visorSuperior.value.length-1)=="x"){
+                auxCalc.value=eval(auxCalc.value+"/"+visorInferior.value)
+            }else{
+                auxCalc.value=eval(auxCalc.value+"*"+visorInferior.value)
+            }   
+            auxBuilderStr.value=""         
+        }
+    }    
+    visorInferior.value = "0"
+    isResult.value="false"
     
 }
